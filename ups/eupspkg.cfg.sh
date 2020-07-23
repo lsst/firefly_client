d47a306172389a27e7a8738d8b375d528a5fedec
Brian Van Klaveren <bvan@slac.stanford.edu>
2020-07-22 23:14:23 -0700

    setuptools fix for eups 2.1.5

---
+++ ups/eupspkg.cfg.sh
@@ -0,0 +1,5 @@
+install() {
+  PYDEST="$PREFIX/lib/python"
+  PYTHONPATH="$PYDEST:$PYTHONPATH" \
+     eval python setup.py install --single-version-externally-managed --record record.txt  $PYSETUP_INSTALL_OPTIONS
+}
