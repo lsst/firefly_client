7f4dd3598bc9968c17a0c42e3bc87a1fc7453ec9
Brian Van Klaveren <bvan@slac.stanford.edu>
2020-07-22 23:16:57 -0700

    don't forget ups directory

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,4 +2,5 @@ install() {
   PYDEST="$PREFIX/lib/python"
   PYTHONPATH="$PYDEST:$PYTHONPATH" \
      eval python setup.py install --single-version-externally-managed --record record.txt  $PYSETUP_INSTALL_OPTIONS
+  install_ups
 }
